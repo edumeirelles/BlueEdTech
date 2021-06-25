1f6c87f6b799462897b1c2db69491a9b5f305ac9
Eduardo <edumeirelles@yahoo.com.br>
2021-06-24 23:11:35 -0300

    Subindo na nuvem!!!

--- Page_Eduardo/static/script.js
+++ Page_Eduardo/static/script.js
@@ -17,8 +17,40 @@ for(const item of barras){
 var menu = document.getElementsByClassName("menu_item");
 
 for (var i = 0; i < menu.length; i++) {
-menu[i].addEventListener("click", function() {
-  (document.querySelector('.selecionado')) ? document.querySelector('.selecionado').classList.remove('selecionado') : '';
-  this.classList.add('selecionado');
-});
+  menu[i].addEventListener("click", function() {
+    (document.querySelector('.selecionado')) ? document.querySelector('.selecionado').classList.remove('selecionado') : '';
+    this.classList.add('selecionado');
+  });
+  menu[i].addEventListener("mousedown", function() {
+    this.classList.remove('menu_item');
+    this.classList.add('click');
+    
+  })
+  menu[i].addEventListener("mouseup", function(){
+    this.classList.add('menu_item');
+    this.classList.remove("click");
+  })
+  menu[i].addEventListener("mouseleave", function(){
+    this.classList.add('menu_item');
+    this.classList.remove("click");
+  })
 }
+
+const divMainPerfil = document.getElementById("divMainPerfil");
+const divMainPortifolio = document.getElementById("divMainPortifolio")
+const portifolio = document.getElementById("portifolio");
+const perfil = document.getElementById("perfil");
+
+function selecionarItem(){
+  if (portifolio.classList.contains('selecionado')){
+    divMainPerfil.classList.add('div_main');
+    divMainPortifolio.classList.remove('div_main')
+  }
+  if (perfil.classList.contains('selecionado')){
+    divMainPerfil.classList.remove('div_main')
+    divMainPortifolio.classList.add('div_main')
+  }
+}
+
+portifolio.addEventListener("click", selecionarItem)
+perfil.addEventListener("click", selecionarItem)
