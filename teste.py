3ac03ff9fdc89d26a07edb4afcbaf513b79c8174
Eduardo Meirelles Nogueira <83734364+edumeirelles@users.noreply.github.com>
2021-10-01 05:00:57 -0300

    Deletando arquivos desnecessários

--- teste.py
+++
@@ -1,9 +0,0 @@
-forma_pg_lista = []
-for i in range(1,37):
-    forma_pg_lista.append((f"1 + {i}",f"1 + {i}"))
-for i in range(1,37):
-    forma_pg_lista.append((f"S/ Entrada + {i}",f"S/ Entrada + {i}"))
-
-forma_pg_tuple = tuple(forma_pg_lista)
-
-print(forma_pg_tuple)
